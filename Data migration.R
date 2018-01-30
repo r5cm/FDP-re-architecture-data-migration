c5b646a5a3481bc6ba57d42a119efe1fd745c8c9
ramirocadavid <ramirocadavid@gmail.com>
2018-01-30 10:27:05 -0500

    Upload up to farm tested + delete records script

--- Data migration.R
+++ Data migration.R
@@ -1,5 +1,5 @@
 
-# 0. Login to Salesforce instances --------------------------------------------------
+# 0. Login to Salesforce instances and gsheets --------------------------------------
 
 # Login to Salesforce admin V1
 library(RForcecom)
@@ -14,6 +14,12 @@ apiversion.2 <- "41.0"
 url.2 <- "https://taroworks-1410--fdpv2.cs58.cloudforce.com/"
 session.2 <- rforcecom.login(username.2, password.2, url.2)
 
+# Access google sheet
+library(googlesheets)
+library(dplyr)
+my_sheets <- gs_ls()
+template.info <- gs_title("V1 to V2 mapping")
+
 # Remove unused objects
 rm(username.1, password.1, username.2, password.2, apiversion.2, url.2)
 
@@ -23,8 +29,8 @@ rm(username.1, password.1, username.2, password.2, apiversion.2, url.2)
 
 # List of objects_V1 to migrate
 fdp.objects.1 <- c("FDP_submission__c", "farmer__c", "farmer_BL__c", "Farm__c", 
-                 "Farm_BL__c", "plot__c", "AODiagnostic__c", "Adoption_observations__c",
-                 "PL_FDP__c", "FDP_calendar__c")
+                   "Farm_BL__c", "plot__c", "AODiagnostic__c", "Adoption_observations__c",
+                   "PL_FDP__c", "FDP_calendar__c")
 # Retrieve objects_V1 meta-data
 metadata.1 <- list()
 for(i in seq_along(fdp.objects.1)) {
@@ -44,10 +50,10 @@ for(i in seq_along(metadata.1)) {
 # RETRIEVE OBJECTS AND FIELDS V2
 
 # List of objects_V2 to insert
-fdp.objects.2 <- c("Submission__c", "Answer__c", "fdp_farmer__c", "fdp_farmer_BL__c",
+fdp.objects.2 <- c("fdp_Submission__c", "Answer__c", "fdp_farmer__c", "fdp_farmer_BL__c",
                    "fdp_Farm__c", "fdp_Farm_BL__c", "fdp_plot__c",
                    "fdp_Diagnostic_Monitoring__c", "fdp_PL__c", "fdp_Calendar__c",
-                   "observation__c")
+                   "fpd_observation__c")
 # Retrieve objects_V2 meta-data
 metadata.2 <- list()
 for(i in seq_along(fdp.objects.2)) {
@@ -66,11 +72,6 @@ for(i in seq_along(metadata.2)) {
 }
 
 # MAP V1 TO V2
-# Access google sheet
-library(googlesheets)
-library(dplyr)
-my_sheets <- gs_ls()
-template.info <- gs_title("V1 to V2 mapping")
 # Load objects and fields to google sheet (WARNING!!!!! ONLY DO THIS ONCE!!!!!!!!)
 gs_edit_cells(template.info, ws = "V1", input = obj.fields.1, col_names = TRUE, trim = TRUE)
 gs_edit_cells(template.info, ws = "V2", input = obj.fields.2, col_names = TRUE)
@@ -82,7 +83,7 @@ gs_edit_cells(template.info, ws = "V2", input = obj.fields.2, col_names = TRUE)
 
 
 # Retrieve v2 to v1 mapping table
-map.v2v1 <- data.frame(gs_read(template.info, "V2", "A1:H329"))
+map.v2v1 <- data.frame(gs_read(template.info, "V2", "A1:H330"))
 # Remove formula fields from map.v2v1
 map.v2v1 <- map.v2v1[map.v2v1$calc_v2 == FALSE, ]
 # Remove fields that won't be loaded (N/A in field_v1 column)
@@ -91,9 +92,9 @@ map.v2v1 <- map.v2v1[map.v2v1$field_v1 != "N/A", ]
 map.v2v1 <- map.v2v1[map.v2v1$empty_v1 == "no", ]
 
 # Vector with objects to load
-obj.2 <- c("Submission__c", "fdp_farmer__c", "fdp_farmer_BL__c", "fdp_Farm__c",
+obj.2 <- c("fpd_Submission__c", "fdp_farmer__c", "fdp_farmer_BL__c", "fdp_Farm__c",
            "fdp_Farm_BL__c", "fdp_plot__c", "fdp_Diagnostic_Monitoring__c", 
-           "observation__c")
+           "fpd_observation__c")
 data.2 <- list()
 fields.ret <- c("object_v2", "field_v2", "type_v2", "object_v1", "field_v1")
 
@@ -135,13 +136,6 @@ saveRDS(data.2, "data_2.RDS")
 # Retrieve data to load
 data.2 <- readRDS("data_2.RDS")
 
-# COUNTRY
-# TBD
-# ...
-# DISTRICTS
-# TBD
-# ...
-
 
 # FAMILY MEMBERS
 
@@ -151,32 +145,50 @@ spouse <- select(data.2$fdp_farmer__c, Id_v1__c, spouseBirthday__c,
 # nombres inventados, completar cuando esté creado
 names(spouse) <- c("Id_v1__c", "Birthdate__c", "educ__c", "Name__c")
 # Generic family member financials
-fm.name <- rep("Generic member", nrow(spouse))
-fm.name <- data.frame(fm.name) #include financials
+fm.name <- rep("Generic member", nrow(data.2$fdp_farmer_BL__c))
+generic.member <- data.frame(fm.name, select(data.2$fdp_farmer_BL__c, )) #include financials
+names(generic.member) <- c("")
 # Create Family members data frame (Pending creation of)
 
-# Remove family members fields from farmer__c
-rm.fields <- c("spouseBirthday__c", "spouseEducationalLevel__c", "spouseName__c")
-data.2$fdp_farmer__c <- select(data.2$fdp_farmer__c, -one_of(rm.fields))
-
+# Remove family members fields from Farmer
+rm.farmer <- c("spouseBirthday__c", "spouseEducationalLevel__c", "spouseName__c",
+               "Spouse_income__c")
+data.2$fdp_farmer__c <- select(data.2$fdp_farmer__c, -one_of(rm.farmer))
+# Remove family members fields from Farmer Baseline
+rm.farmerbl <- c("dependEconomically__c", "familyMembers__c", "familyMembersPaidWork__c",
+                 "receivesPaymentFarmLabor__c", "spouseIncome__c", "Have_spouse__c",
+                 "spouseHavePaidWork__c", "familyMembersIncome__c")
+data.2$fdp_farmer_BL__c <- select(data.2$fdp_farmer_BL__c, -one_of(rm.farmerbl))
 # Add family members to data.2
+data.2[[length(data.2) + 1]] <- c()
 
 
 # FARMER(ALL fields except submission)
 
 # Organization: all to Mars
-data.2$fdp_farmer__c$organization__c <- "a0l0l000000FY1M"
-# Village: all to Test Village while villages are created
-data.2$fdp_farmer__c$village__c <- "a0k0l000004kmlv"
+data.2$fdp_farmer__c$organization__c <- "a1M0l0000002FbKEAU"
+
+# Village:
+id.villages.2 <- rforcecom.retrieve(session.2, "fpd_village__c", c("Id", "Name"))
+names(id.villages.2) <- c("Id.village.2", "Name")
+temp.villages <- left_join(data.2$fdp_farmer__c, id.villages.2, 
+                           by = c("village_v1__c" = "Name"))
+data.2$fdp_farmer__c$village__c <- temp.villages$Id.village.2
+rm(temp.villages)
+      
+# Status
+data.2$fdp_farmer__c$status__c <- "Active"
+# User (only for test, delete for production migration, already contained in data.2)
+data.2$fdp_farmer__c$OwnerId <- "0050K000007ts87QAA"
+
 # Run insert job
 job_info <- rforcecom.createBulkJob(session.2, 
-                                    operation='insert', 
+                                    operation='insert',
                                     object='fdp_farmer__c')
 # Insert
 batches_info <- rforcecom.createBulkBatch(session.2, 
                                           jobId=job_info$id, 
-                                          select(data.2$fdp_farmer__c[test.rec, ],
-                                                 -one_of(new.empty)),
+                                          data.2$fdp_farmer__c,
                                           multiBatch = TRUE, 
                                           batchSize=500)
 # check on status of each batch
@@ -197,19 +209,251 @@ batches_detail <- lapply(batches_info,
 close_job_info <- rforcecom.closeBulkJob(session.2, jobId=job_info$id)
 
 # SUBMISSION
-# Submissions(status) - CONFIRM WITH AAN
-# Submissions(farmer)
-# Load submissions
+# Status - CONFIRM WITH AAN
+# farmer
+id.farmer.2 <- rforcecom.retrieve(session.2, "fdp_farmer__c", c("Id", "Id_v1__c"))
+names(id.farmer.2) <- c("Id.farmer.2", "Id.farmer.1")
+library(dplyr)
+temp.sub <- left_join(data.2$fpd_Submission__c, id.farmer.2, 
+                      by = c("Respondent_v1__c" = "Id.farmer.1"))
+data.2$fpd_Submission__c$Respondent__c <- temp.sub$Id.farmer.2
+rm(temp.sub)
+# surveyor: DELETE THIS PART WHEN IN PRODUCTION INSTANCE
+data.2$fpd_Submission__c$Surveyor__c <- "0050K000007ts87QAA"
 
-# FARMER
-# Farmer(submission)
-subm.12 <- rforcecom.retrieve(session.2, "Submission__c", c("Id", "Id_v1__c"))
-data.2$fdp_farmer__c$FDP_submission__c <- left_join(data.2$fdp_farmer__c, subm.12,
-                                                    by = "Id_v1__c")[, "FPD_submission__c"]
-# Farmer(organization)
-# Farmer(status = Active)
+# Load submissions
+# Run insert job
+job_info <- rforcecom.createBulkJob(session.2, 
+                                    operation='insert',
+                                    object='fpd_Submission__c')
+# Insert
+batches_info <- rforcecom.createBulkBatch(session.2, 
+                                          jobId=job_info$id, 
+                                          data.2$fpd_Submission__c,
+                                          multiBatch = TRUE, 
+                                          batchSize=500)
+# check on status of each batch
+batches_status <- lapply(batches_info, 
+                         FUN=function(x){
+                               rforcecom.checkBatchStatus(session.2, 
+                                                          jobId=x$jobId, 
+                                                          batchId=x$id)
+                         })
+# get details on each batch
+batches_detail <- lapply(batches_info, 
+                         FUN=function(x){
+                               rforcecom.getBatchDetails(session.2, 
+                                                         jobId=x$jobId, 
+                                                         batchId=x$id)
+                         })
+# Close job
+close_job_info <- rforcecom.closeBulkJob(session.2, jobId=job_info$id)
+
+
+# FARMER (II)
+# submission
+id.subm.2 <- rforcecom.retrieve(session.2, "fpd_Submission__c", 
+                                c("Id", "Id_v1__c", "Respondent__c"))
+names(id.subm.2) <- c("Id.subm.2", "Id.subm.1", "Respondent__c")
+temp.farmersub <- left_join(data.2$fdp_farmer__c, id.subm.2,
+                                                    by = c("FDP_submission_v1__c" = "Id.subm.1"))
+data.2$fdp_farmer__c$FDP_submission__c <- temp.farmersub$Id.subm.2
+rm(temp.farmersub)
+
+# data to update
+farmers.update <- left_join(id.farmer.2, id.subm.2, 
+                            by = c("Id.farmer.2" = "Respondent__c"))
+farmers.update <- select(farmers.update, Id.farmer.2, Id.subm.2)
+names(farmers.update) <- c("Id", "FDP_submission__c")
+
+# update farmers with submission
+# Run update job
+job_info <- rforcecom.createBulkJob(session.2, 
+                                    operation='update',
+                                    object='fdp_farmer__c')
+# Insert
+batches_info <- rforcecom.createBulkBatch(session.2, 
+                                          jobId=job_info$id, 
+                                          farmers.update,
+                                          multiBatch = TRUE, 
+                                          batchSize=500)
+# check on status of each batch
+batches_status <- lapply(batches_info, 
+                         FUN=function(x){
+                               rforcecom.checkBatchStatus(session.2, 
+                                                          jobId=x$jobId, 
+                                                          batchId=x$id)
+                         })
+# get details on each batch
+batches_detail <- lapply(batches_info, 
+                         FUN=function(x){
+                               rforcecom.getBatchDetails(session.2, 
+                                                         jobId=x$jobId, 
+                                                         batchId=x$id)
+                         })
+# Close job
+close_job_info <- rforcecom.closeBulkJob(session.2, jobId=job_info$id)
+
+
+# FARM - Farmer and submission
+# Prepare farms to load
+# Submission
+temp.farmsub <- left_join(data.2$fdp_Farm__c, id.subm.2, 
+                          by = c("FDP_submission_v1__c" = "Id.subm.1"))
+data.2$fdp_Farm__c$FDP_submission__c <- temp.farmsub$Id.subm.2
+rm(temp.farmsub)
+# Farmer
+temp.farmfarmer <- left_join(data.2$fdp_Farm__c, id.farmer.2,
+                             by = c("farmer_v1__c" = "Id.farmer.1"))
+data.2$fdp_Farm__c$farmer__c <- temp.farmfarmer$Id.farmer.2
+# Load farms
+# Run insert job
+job_info <- rforcecom.createBulkJob(session.2, 
+                                    operation='insert',
+                                    object='fdp_Farm__c')
+# Insert
+batches_info <- rforcecom.createBulkBatch(session.2, 
+                                          jobId=job_info$id, 
+                                          data.2$fdp_Farm__c,
+                                          multiBatch = TRUE, 
+                                          batchSize=500)
+# check on status of each batch
+batches_status <- lapply(batches_info, 
+                         FUN=function(x){
+                               rforcecom.checkBatchStatus(session.2, 
+                                                          jobId=x$jobId, 
+                                                          batchId=x$id)
+                         })
+# get details on each batch
+batches_detail <- lapply(batches_info, 
+                         FUN=function(x){
+                               rforcecom.getBatchDetails(session.2, 
+                                                         jobId=x$jobId, 
+                                                         batchId=x$id)
+                         })
+# Close job
+close_job_info <- rforcecom.closeBulkJob(session.2, jobId=job_info$id)
+
+
+# FARMER (III) - update farm
+# Prepare farmers to update
+id.farm.2 <- rforcecom.retrieve(session.2, "fdp_Farm__c", c("Id", "Id_v1__c", "farmer__c"))
+names(id.farm.2) <- c("Id.farm.2", "Id.farm.1", "Id.farmer.2")
+temp.farmerfarm <- left_join(id.farmer.2, id.farm.2, by =  "Id.farmer.2")
+farmers.update2 <- select(temp.farmerfarm, Id.farmer.2, Id.farm.2)
+names(farmers.update2) <- c("Id", "FDP_Farm__c")
+# Update farmers
+# Run update job
+job_info <- rforcecom.createBulkJob(session.2, 
+                                    operation='update',
+                                    object='fdp_farmer__c')
+# Insert
+batches_info <- rforcecom.createBulkBatch(session.2, 
+                                          jobId=job_info$id, 
+                                          farmers.update2,
+                                          multiBatch = TRUE, 
+                                          batchSize=500)
+# check on status of each batch
+batches_status <- lapply(batches_info, 
+                         FUN=function(x){
+                               rforcecom.checkBatchStatus(session.2, 
+                                                          jobId=x$jobId, 
+                                                          batchId=x$id)
+                         })
+# get details on each batch
+batches_detail <- lapply(batches_info, 
+                         FUN=function(x){
+                               rforcecom.getBatchDetails(session.2, 
+                                                         jobId=x$jobId, 
+                                                         batchId=x$id)
+                         })
+# Close job
+close_job_info <- rforcecom.closeBulkJob(session.2, jobId=job_info$id)
+
+
+# FARMER BASELINE
+# farmer
+temp.fblfarmer <- left_join(data.2$fdp_farmer_BL__c, id.farmer.2,
+                            by =c("farmer_v1__c" = "Id.farmer.1"))
+data.2$fdp_farmer_BL__c$farmer__c <- temp.fblfarmer$Id.farmer.2
+rm(temp.fblfarmer)
+# submission
+temp.fblsub <- left_join(data.2$fdp_farmer_BL__c, id.subm.2,
+                         by = c("FDP_submission_v1__c" = "Id.subm.1"))
+data.2$fdp_farmer_BL__c$FDP_submission__c <- temp.fblsub$Id.subm.2
+rm(temp.fblsub)
+# Delete duplicated farmer baselines
+unique.fbl <- !duplicated(data.2$fdp_farmer_BL__c$farmer_v1__c)
+data.2$fdp_farmer_BL__c <- data.2$fdp_farmer_BL__c[unique.fbl, ]
+# load farmer baselines
+# Run insert job
+job_info <- rforcecom.createBulkJob(session.2, 
+                                    operation='insert',
+                                    object='fdp_farmer_BL__c')
+# Insert
+batches_info <- rforcecom.createBulkBatch(session.2, 
+                                          jobId=job_info$id, 
+                                          data.2$fdp_farmer_BL__c,
+                                          multiBatch = TRUE, 
+                                          batchSize=500)
+# check on status of each batch
+batches_status <- lapply(batches_info, 
+                         FUN=function(x){
+                               rforcecom.checkBatchStatus(session.2, 
+                                                          jobId=x$jobId, 
+                                                          batchId=x$id)
+                         })
+# get details on each batch
+batches_detail <- lapply(batches_info, 
+                         FUN=function(x){
+                               rforcecom.getBatchDetails(session.2, 
+                                                         jobId=x$jobId, 
+                                                         batchId=x$id)
+                         })
+# Close job
+close_job_info <- rforcecom.closeBulkJob(session.2, jobId=job_info$id)
+
+
+# FARM BASELINE
+# farm
+temp.farmblfarm <- left_join(data.2$fdp_Farm_BL__c, id.farm.2,
+                             by = c("farm_v1__c" = "Id.farm.1"))
+data.2$fdp_Farm_BL__c$farm__c <- temp.farmblfarm$Id.farm.2
+rm(temp.farmblfarm)
+# submission
+temp.farmsub <- left_join(data.2$fdp_Farm_BL__c, id.subm.2,
+                          by = c("FDP_submission_v1__c" = "Id.subm.1"))
+data.2$fdp_Farm_BL__c$FDP_submission__c <- temp.farmsub$Id.subm.2
+rm(temp.farmsub)
+# Load farm baselines
+# Run insert job
+job_info <- rforcecom.createBulkJob(session.2, 
+                                    operation='insert',
+                                    object='fdp_Farm_BL__c')
+# Insert
+batches_info <- rforcecom.createBulkBatch(session.2, 
+                                          jobId=job_info$id, 
+                                          data.2$fdp_Farm_BL__c,
+                                          multiBatch = TRUE, 
+                                          batchSize=500)
+# check on status of each batch
+batches_status <- lapply(batches_info, 
+                         FUN=function(x){
+                               rforcecom.checkBatchStatus(session.2, 
+                                                          jobId=x$jobId, 
+                                                          batchId=x$id)
+                         })
+# get details on each batch
+batches_detail <- lapply(batches_info, 
+                         FUN=function(x){
+                               rforcecom.getBatchDetails(session.2, 
+                                                         jobId=x$jobId, 
+                                                         batchId=x$id)
+                         })
+# Close job
+close_job_info <- rforcecom.closeBulkJob(session.2, jobId=job_info$id)
+
 
 # AO DIAGNOSTIC
 
 # Type (all = 'Diagnostic')
-
---
+++ delete records.R
@@ -0,0 +1,29 @@
+object <- "fdp_farmer_BL__c"
+delete <- rforcecom.retrieve(session.2, object, "Id")
+
+# Run delete job
+job_info <- rforcecom.createBulkJob(session.2, 
+                                    operation='delete',
+                                    object= object)
+# Insert
+batches_info <- rforcecom.createBulkBatch(session.2, 
+                                          jobId=job_info$id, 
+                                          delete,
+                                          multiBatch = TRUE, 
+                                          batchSize=500)
+# check on status of each batch
+batches_status <- lapply(batches_info, 
+                         FUN=function(x){
+                               rforcecom.checkBatchStatus(session.2, 
+                                                          jobId=x$jobId, 
+                                                          batchId=x$id)
+                         })
+# get details on each batch
+batches_detail <- lapply(batches_info, 
+                         FUN=function(x){
+                               rforcecom.getBatchDetails(session.2, 
+                                                         jobId=x$jobId, 
+                                                         batchId=x$id)
+                         })
+# Close job
+close_job_info <- rforcecom.closeBulkJob(session.2, jobId=job_info$id)
